643b46cab18849808dd16fd8e874b188364e90c4
Oleg <pyatak.ov@yandex.ru>
2023-01-21 10:36:37 +0300

    add firebase auth plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    runtimeConfig: {
+        FIREBASE_API_KEY: process.env.API_KEY,
+        public: {}
+    },
+});
